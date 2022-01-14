7a5cc741a9d0b9a106284ceb0e41eee85c1b0bba
Frail Bongat <frail.bongat@gmail.com>
2022-01-14 20:22:10 +0800

    📝 Add to lesson 13

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,42 @@
 <script>
-  let name = 'Yoshi';
-	let beltColour = 'black';
+	import Modal from './Modal.svelte'
 
-	const handleClick = () => {
-		beltColour = 'orange';
+	let showModal = false
+
+	const toggleModal = () => {
+		showModal = !showModal
 	}
+
+	let people = [
+		{ name: 'yoshi', beltColour: 'black', age: 25, id: 1},
+		{ name: 'mario', beltColour: 'orange', age: 45, id: 2},
+		{ name: 'luigi', beltColour: 'brown', age: 35, id: 3},
+	]
+
+	const handleClick = (id) => {
+		people = people.filter(person => person.id !== id)
+	}
+
+	let num = 5
 </script>
 
+<Modal message='Hey, I am a prop value' {showModal} on:click={toggleModal} />
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>{beltColour} belt</p>
-	<button on:click={handleClick}>update belt colour</button>
+	<button on:click|once={toggleModal}>Open Modal</button>
+
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			<p>{person.age} years old, {person.beltColour} belt.</p>
+			{#if person.beltColour === 'black'}
+				<p><strong>MASTER NINJA</strong></p>
+			{/if}
+			<button on:click={() => handleClick(person.id)}>delete</button>
+		</div>
+	{:else}
+		<p>There are no people to show...</p>
+	{/each}
 </main>
 
 <style>
@@ -19,13 +45,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
